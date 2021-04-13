d1f1409ffee9c6bec72bd22de270696d36a5b0b0
Synz75 <Qc_Angelz@hotmail.fr>
2021-04-13 14:09:16 -0400

    Add files via upload

---
+++ Platinum_Bars.sql
@@ -0,0 +1,24 @@
+/*
+SQLyog Community v13.1.6 (64 bit)
+MySQL - 5.7.24 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `platinum_bars` (
+	`guid` int (10),
+	`name` varchar (36),
+	`race` tinyint (3),
+	`class` tinyint (3),
+	`platinum_amount` int (10),
+	`platinum_pieces` int (10)
+); 
+insert into `platinum_bars` (`guid`, `name`, `race`, `class`, `platinum_amount`, `platinum_pieces`) values('1','Syn','4','4','30','33');
+insert into `platinum_bars` (`guid`, `name`, `race`, `class`, `platinum_amount`, `platinum_pieces`) values('9','Elfyiaz','4','11','1','5');
+insert into `platinum_bars` (`guid`, `name`, `race`, `class`, `platinum_amount`, `platinum_pieces`) values('36','Kioze','11','7','23','2');
+insert into `platinum_bars` (`guid`, `name`, `race`, `class`, `platinum_amount`, `platinum_pieces`) values('37','Magox','11','8','8','0');
+insert into `platinum_bars` (`guid`, `name`, `race`, `class`, `platinum_amount`, `platinum_pieces`) values('43','Anhan','6','11','4','0');
+insert into `platinum_bars` (`guid`, `name`, `race`, `class`, `platinum_amount`, `platinum_pieces`) values('51','Yaniene','5','5','1','0');
+insert into `platinum_bars` (`guid`, `name`, `race`, `class`, `platinum_amount`, `platinum_pieces`) values('53','Mann','4','4','213','0');
+insert into `platinum_bars` (`guid`, `name`, `race`, `class`, `platinum_amount`, `platinum_pieces`) values('58','Part','7','8','1','0');
+insert into `platinum_bars` (`guid`, `name`, `race`, `class`, `platinum_amount`, `platinum_pieces`) values('76','Asuulo','11','1','27','0');
